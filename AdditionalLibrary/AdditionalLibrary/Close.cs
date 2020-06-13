c6c0be5bd2e828e122281e823a58193c0e75cba0
OlesNovikov <55893703+OlesNovikov@users.noreply.github.com>
2020-06-13 12:05:14 +0300

    Library with additional method

---
+++ AdditionalLibrary/AdditionalLibrary/Close.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Sockets;
+using System.Threading;
+
+namespace AdditionalLibrary
+{
+    public static class Close
+    {
+        public static void CloseSocket(ref Socket socket)
+        {
+            if (socket != null)
+            {
+                socket.Close();
+                socket = null;
+            }
+        }
+
+        public static void CloseThread(ref Thread thread)
+        {
+            if (thread != null)
+            {
+                thread.Abort();
+                thread = null;
+            }
+        }
+    }
+}
