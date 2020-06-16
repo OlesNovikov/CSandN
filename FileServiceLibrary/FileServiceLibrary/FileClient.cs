23bd1666f173863e3dbaaface53d3a9228e617f4
OlesNovikov <55893703+OlesNovikov@users.noreply.github.com>
2020-06-17 00:56:10 +0300

    Added FileClient

---
+++ FileServiceLibrary/FileServiceLibrary/FileClient.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FileServiceLibrary
+{
+    public class FileClient
+    {
+        private const string SERVER_URI = "http://localhost:8888/";
+        private const int MB = 1024 * 1024;
+        private const int MAX_FILE_SIZE = 5 * MB;
+        private const int MAX_TOTAL_SIZE = 3 * MAX_FILE_SIZE;
+
+        private Dictionary<int, string> DictionaryOfFiles;
+        private List<string> ListOfFilesExtensions = new List<string>() { ".txt", ".docx", ".png", ".jpg", ".jpeg", ".pdf" };
+        private int TotalSize = 0;
+
+        public FileClient()
+        {
+            DictionaryOfFiles = new Dictionary<int, string>();
+        }
+
+        public bool SizeFits(int fileSize)
+        {
+            int totalSize = TotalSize;
+            if ((fileSize <= MAX_FILE_SIZE) && ((TotalSize += fileSize) <= MAX_TOTAL_SIZE)) return true;
+            else return false;
+        }
+
+        public bool ExtensionExists(string extension)
+        {
+            if (ListOfFilesExtensions.Exists(x => x.Contains(extension))) return true;
+            else return false;
+        }
+
+        private string UniqueFileName(string fileName)
+        {
+            string randomFileName = Path.GetRandomFileName();
+            string uniqueFileName = randomFileName.Substring(0, 8) + "_" + fileName;
+            return uniqueFileName;
+        }
+
+        private MultipartFormDataContent MIMEEncodedContent(string filePath)
+        {
+            MultipartFormDataContent encodedContent = new MultipartFormDataContent();
+            ByteArrayContent byteArrayContent;
+            byte[] byteArray;
+
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                int fileSize = (int)fileStream.Length;
+                byteArray = new byte[fileSize];
+                fileStream.Read(byteArray, 0, fileSize);
+            }
+            byteArrayContent = new ByteArrayContent(byteArray);
+            encodedContent.Add(byteArrayContent);
+            return encodedContent;
+        }
+
+        public async Task<int> LoadFileToService(string filePath)
+        {
+            const int ERROR_CODE = -200;
+            try
+            {
+                string fileName = Path.GetFileName(filePath);
+                string fileExtension = Path.GetExtension(filePath);
+
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, SERVER_URI + UniqueFileName(fileName));
+                    httpRequestMessage.Content = MIMEEncodedContent(filePath);
+                    httpRequestMessage.Headers.Add("FileName", fileName);
+
+                    HttpResponseMessage httpResponseMessage = await client.SendAsync(httpRequestMessage);
+                    string fileID = await httpResponseMessage.Content.ReadAsStringAsync();
+                    return int.Parse(fileID);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("public async Task<int> LoadFileToService(string filePath)" + exception.Message);
+                return ERROR_CODE;
+            }
+        }
+    }
+}
---
+++ FileServiceLibrary/FileServiceLibrary/FileServer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileServiceLibrary
+{
+    public class FileServer
+    {
+        private HttpListener httpListener;
+        private Dictionary<int, string> DictionaryOfFiles;
+        private Thread httpListenThread;
+        private int maxFileID = 0;
+        
+        public FileServer(string prefix)
+        {
+            httpListener = new HttpListener();
+            httpListener.Prefixes.Add(prefix);
+            DictionaryOfFiles = new Dictionary<int, string>();
+            httpListenThread = new Thread(ListenHttp);
+        }
+
+        private void ListenHttp()
+        {
+            httpListener.Start();
+            Console.WriteLine("Server start listen HTTP requests...");
+
+            while (true)
+            {
+                HttpListenerContext listenerContext = httpListener.GetContext();
+                Console.WriteLine(DateTime.Now.ToShortTimeString() + " " + listenerContext.Request.HttpMethod.ToString());
+                IdentifyHttpRequest(listenerContext);
+            }
+        }
+
+        private void IdentifyHttpRequest(HttpListenerContext listenerContext)
+        {
+            const string POST = "POST";
+            const string GET = "GET";
+            const string DELETE = "DELETE";
+            const string HEAD = "HEAD";
+
+            string httpMethod = listenerContext.Request.HttpMethod;
+
+            if (httpMethod == POST) HandlePOSTMethod(listenerContext);
+            if (httpMethod == GET) HandleGETMethod(listenerContext);
+            if (httpMethod == HEAD) HandleHEADMethod(listenerContext);
+            if (httpMethod == DELETE) HandleDELETEMethod(listenerContext);
+        }
+
+        private void HandlePOSTMethod(HttpListenerContext listenerContext)
+        {
+            string fileName = listenerContext.Request.Url.LocalPath.Substring(1);
+            Stream inputStream = listenerContext.Request.InputStream;
+            Encoding contentEncoding = listenerContext.Request.ContentEncoding;
+
+            StreamReader reader = new StreamReader(inputStream, contentEncoding);
+            string requestContent = reader.ReadToEnd();
+            byte[] fileContent = listenerContext.Request.ContentEncoding.GetBytes(requestContent);
+            DictionaryOfFiles.Add(maxFileID, fileContent);
+        }
+
+        private void HandleGETMethod(HttpListenerContext listenerContext)
+        {
+
+        }
+
+        private void HandleHEADMethod(HttpListenerContext listenerContext)
+        {
+
+        }
+
+        private void HandleDELETEMethod(HttpListenerContext listenerContext)
+        {
+
+        }
+    }
+}
