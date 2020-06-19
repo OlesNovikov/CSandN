8af003e1433f3db157ed3663c4264840ef37a234
OlesNovikov <55893703+OlesNovikov@users.noreply.github.com>
2020-06-19 21:41:39 +0300

    Added file storage setup method

---
+++ AdditionalLibrary/AdditionalLibrary/FileStorage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalLibrary
+{
+    public static class FileStorage
+    {
+        public static void SetupStorage(string fileStoragePath)
+        {
+            if (Directory.Exists(fileStoragePath))
+            {
+                DirectoryInfo fileStorageDirectory = new DirectoryInfo(fileStoragePath);
+                foreach (var file in fileStorageDirectory.GetFiles()) file.Delete();
+            }
+            else Directory.CreateDirectory(fileStoragePath);
+        }
+    }
+}
